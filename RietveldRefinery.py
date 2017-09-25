b4bbe2a78aba930f579caa809cd1ca6637c31dc8
M@ <mwilliams@protoxrd.com>
2017-09-25 10:48:26 -0400

    Adding Refinery module

---
+++ RietveldRefinery.py
@@ -0,0 +1,24 @@
+from __future__ import division
+import os, random, math
+import iotbx.cif, cctbx.miller
+import scitbx
+from cctbx import xray
+from cctbx import crystal
+from cctbx.array_family import flex
+from cctbx.eltbx import wavelengths
+import time
+import sys, subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+from scitbx import lbfgsb
+import jsonpickle
+from libtbx import easy_pickle
+
+class RietveldRefinery:
+   """
+      This class is used to assemble and organize the inputs required to run a
+      series of Rietveld refinements, as per some specifications loaded from
+      a file/string.
+   """
+
+   def    
